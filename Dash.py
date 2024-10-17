a3b90e64b62be75cb919171a97a8f5bd6f34d1ae
urudzi <cybercuvee@gmail.com>
2024-10-18 00:50:57 +0300

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -38,7 +38,7 @@ async def button(update: Update, context):
 
 # Основная функция запуска бота
 async def main():
-    application = ApplicationBuilder().token("7836847076:AAHBqpnjUqL-E9yUmMo9FtE2Za_kWKsUdz8").build()
+    application = ApplicationBuilder().token("7836847076:AAHVcgjHmMErXbyUIcqt__PpceYyHfraoi8").build()
 
     # Обработчики команд
     application.add_handler(CommandHandler("start", start))
